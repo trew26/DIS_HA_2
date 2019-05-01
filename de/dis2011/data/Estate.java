bda987dc7495d01e66fd08e784868d17724ea76d
MiddyGoesDev <finnrietz@googlemail.com>
2019-05-01 12:05:11 +0200

    getter and setter methods for estate

--- de/dis2011/data/Estate.java
+++ de/dis2011/data/Estate.java
@@ -17,5 +17,59 @@ public class Estate {
     private String area;
     private String fk_agent;
 
+    public int getId() {
+        return id;
+    }
 
+    public int getzip() {
+        return zip;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public String getFk_agent() {
+        return fk_agent;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public void setFk_agent(String fk_agent) {
+        this.fk_agent = fk_agent;
+    }
 }
